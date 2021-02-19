e7a771eb455ab62c94bf22aa9247bd4a7e547ced
Mike Tarpey <miketarpey@gmx.net>
2021-02-19 19:40:00 +0000

    added get_sample_sales to .factory module

--- piper/factory.py
+++ piper/factory.py
@@ -1,10 +1,33 @@
 import pandas as pd
 import numpy as np
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def get_sample_data(start='2020', end='2021', freq='D', seed=30):
-    ''' NOTE:: This function for use when testing in notebooks - nothing to do
-               with PYTESTING
+    ''' NOTE:: This function for use when testing in notebooks
+                - nothing to do with PYTESTING
+
+    Example
+    -------
+    df = get_sample_data()
+
+
+    Parameters
+    ----------
+    start: int - start year
+
+    end: int - end year
+
+    freq: str - frequency (default 'D' days)
+
+    seed: int - random seed, default 42
+
+
+    Returns
+    -------
+    Pandas Dataframe
     '''
 
     if seed is not None:
@@ -41,5 +64,64 @@ def get_sample_data(start='2020', end='2021', freq='D', seed=30):
     }
 
     df = pd.DataFrame(data_dictionary)
+
+    return df
+
+
+def get_sample_sales(number_of_rows = 200, year=2021):
+    '''
+    Generate sales product test data for given year:
+
+    location, product, month, target_sales,
+    target_profit, actual_sales, actual profit.
+
+    Example
+    -------
+    df = get_sample_sales()
+
+
+    Parameters
+    ----------
+    number_of_rows - int - number of records/rows required.
+
+    year - int - sales year to generate data for.
+
+    seed: int - random seed, default 42
+
+
+    Returns
+    -------
+    Pandas Dataframe
+
+    '''
+
+    locations = ['London', 'Paris', 'Milan']
+    products = ['Tops & Blouses', 'Jeans', 'Footwear',
+                'Beachwear', 'Sportswear']
+
+    data = {
+        'location': np.random.choice(locations, size=number_of_rows),
+        'product': np.random.choice(products, size=number_of_rows),
+        'month': np.random.choice(range(1, 13), size=number_of_rows),
+        'target_sales': np.random.randint(14000, 40000, size=number_of_rows),
+        'target%_profit': np.random.randint(10, size=number_of_rows) * .02
+    }
+
+    df = pd.DataFrame(data)
+
+    df['month'] = df['month'].apply(
+        lambda x: pd.Period(f'{year}-{str(x).zfill(2)}'))
+
+    df['target_profit'] = df['target_sales'] * df['target%_profit']
+
+    actual_sales_lambda = lambda x: x + (x * np.random.choice(range(-10, 10)) / 100)
+    df['actual_sales'] = df['target_sales'].apply(actual_sales_lambda)
+
+    df['actual_profit'] = (df['actual_sales'] * df['target%_profit']).round(2)
+
+    df.drop(columns=['target%_profit'], inplace=True)
+    df.month = pd.PeriodIndex(df.month).to_timestamp()
+
+    df = df.sort_values(['month', 'location', 'product'])
 
     return df
